6ad86a00608cb56ba05ddc147fc2426ad9c9e943
dou0203dou0203 <dou0203dou0203@users.noreply.github.com>
2026-03-04 21:46:03 +0900

    feat: GitHub Pages deployment setup

--- src/App.tsx
+++ src/App.tsx
@@ -8,7 +8,7 @@ import SurveyMobile from './pages/SurveyMobile';
 
 function App() {
   return (
-    <BrowserRouter basename="/talent">
+    <BrowserRouter basename="/talentmanagementdemo">
       <Routes>
         {/* Admin Layout */}
         <Route path="/" element={<Layout />}>
